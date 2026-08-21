e30d9077ed442d7def9a734e786b0ac01d7eb295
kaladiyarohaan-png <kaladiyarohaan@gmail.com>
2026-08-21 12:51:06 +0530

    Create streamlit_app.py
    Signed-off-by: kaladiyarohaan-png <kaladiyarohaan@gmail.com>

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+import random
+
+number = random.randint(1, 100)
+attempts = 0
+
+print("🎮 NUMBER GUESSING GAME")
+print("I have selected a number between 1 and 100.")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < number:
+            print("📈 Too low! Try again.")
+        elif guess > number:
+            print("📉 Too high! Try again.")
+        else:
+            print("🎉 CORRECT!")
+            print("You guessed the number in", attempts, "attempts.")
+            break
+
+    except ValueError:
+        print("❌ Please enter a number.")
